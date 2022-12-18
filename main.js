c0d4c19c8cf0c2dcf3547df3e3aff762508cb8a3
Irfan Hariyanto <78160909+rtwone@users.noreply.github.com>
2022-12-18 10:01:14 +0700

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get('/jasa-coding', (req, res) => {
-   const longUrl = 'https://wa.me/19292929999'
-   res.redirect(longUrl)
-})
-
-app.use('/', router)
